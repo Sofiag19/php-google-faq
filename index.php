86b5aece013b56fce5208f1c5b5d36465296d989
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-12-09 22:15:46 +0100

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <!-- Riscrivere questa pagina del sito google
+    https://policies.google.com/faq.
+    Ci sono diverse domande con relative risposte.
+    Gestire il "Database" e la visualizzazione di
+    queste domande e risposte con PHP. -->
+  </body>
+</html>
